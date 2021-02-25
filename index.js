db83eae28872ba144c64b6303922fe52e7ece244
Jenna Qian <jiayin1996@gmail.com>
2021-02-25 16:17:53 -0500

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function rollDice() {
-    let input = document.getElementById("dice-input")
-    let diceInput = input.value
+    let diceInput = document.getElementById("dice-input")
+    let input = diceInput.value
 
     let possibleOutcomes = ["&#9856 = ⚀", "&#9857 = ⚁", "&#9858 = ⚂", "&#9859 = ⚃", "&#9860 = ⚄", "&#9861 = ⚅"]
-    
+    let sum = 0
+
     for(let i = 1; i <= input; i++) {
-        
+        let results = (Math.round(Math.random * 6))
+        sum = sum + results
     }
+  
+    let uList = document.querySelector("dice-history")
+    let liHistory = document.createElement('li')
+    ul.appendChild(liHistory)
 }
 
