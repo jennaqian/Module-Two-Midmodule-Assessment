c40a3077faf452ee59ad651fea5d4592ad93233f
Jenna Qian <jiayin1996@gmail.com>
2021-02-25 12:08:45 -0500

    index.css

---
+++ index.js
@@ -0,0 +1,11 @@
+function rollDice() {
+    let input = document.getElementById("dice-input")
+    let diceInput = input.value
+
+    let possibleOutcomes = ["&#9856 = ⚀", "&#9857 = ⚁", "&#9858 = ⚂", "&#9859 = ⚃", "&#9860 = ⚄", "&#9861 = ⚅"]
+    
+    for(let i = 1; i <= input; i++) {
+        
+    }
+}
+
